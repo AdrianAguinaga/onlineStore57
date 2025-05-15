d58447e2c892601ffb75e7abf1beecbf1702cc5b
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-05-14 19:00:59 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  // This is the main component of the application
+  return (
+    <>
+    <Navbar />
+    <h1>Hello world from React.js (App.jsx)</h1>
+   
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+// Imports section
+import './Home.css'
+
+// Functional component
+function Home() {
+  return(
+    <div className="home">
+
+      <div className="home-text">
+        <h1>Organika (Home.jsx)</h1>
+        <h6>Fresh food, healthier life.</h6>
+      </div> 
+      
+    </div>
+  )
+}
+
+// Export section
+export default Home
